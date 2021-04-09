642fe6363b4153b4707a92cb12695bddd76bfebd
Harshit-Singh222 <74787231+Harshit-Singh222@users.noreply.github.com>
2021-04-09 17:00:50 +0530

    u

---
+++ indexjs.js
@@ -0,0 +1,8 @@
+var height = document.body.clientWidth;
+if(height<=750){
+    document.getElementById('one').className = 'col-12'
+    document.getElementById('two').className = 'col-12'
+}
+else{
+    console.log(height)
+}
